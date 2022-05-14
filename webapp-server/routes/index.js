a4a1287738e0f7baddb39b02d92d28e2ef532464
Praniis <praniis1234@gmail.com>
2022-05-14 20:11:02 +0530

    removed default value

--- webapp-server/routes/index.js
+++ webapp-server/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/app.json')[env];
+
 router.get('/', function (req, res) {
-    res.render('home', {});
+    res.render('home', {
+        mapAPI: app.API["mapAPI"]
+    });
 });
 
 module.exports = router;
